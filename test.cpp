38ec4879c86017db73e855ad8c04831ff4bae886
Nguyễn Quang Dũng <yungngq@gmail.com>
2025-10-02 11:14:47 +0700

    add decision table's testcases

--- test.cpp
+++ test.cpp
@@ -9,12 +9,25 @@ const std::string NO = "No Award";
 TEST(CalculateLoginAwardTest, KTGTBien) {
     EXPECT_EQ(calculateLoginAward(0, false, 50), NORMAL);
     EXPECT_EQ(calculateLoginAward(1, false, 50), NORMAL);
-    EXPECT_EQ(calculateLoginAward(99, false, 50), BIG);
+    EXPECT_EQ(calculateLoginAward(99, false, 50), NORMAL);
     EXPECT_EQ(calculateLoginAward(100, false, 50), BIG);
     EXPECT_EQ(calculateLoginAward(50, false, 0), NORMAL);
     EXPECT_EQ(calculateLoginAward(50, false, 1), NORMAL);
-    EXPECT_EQ(calculateLoginAward(50, false, 99), BIG);
+    EXPECT_EQ(calculateLoginAward(50, false, 99), NORMAL);
     EXPECT_EQ(calculateLoginAward(50, false, 100), BIG);
     EXPECT_EQ(calculateLoginAward(50, true, 50), BIG);
     EXPECT_EQ(calculateLoginAward(50, false, 50), NORMAL);
+}
+
+TEST(CalculateLoginAwardTest, BangQuyetDinh) {
+    EXPECT_EQ(calculateLoginAward(100, true, 50), LEGENDARY);
+    EXPECT_EQ(calculateLoginAward(100, false, 50), BIG);
+    EXPECT_EQ(calculateLoginAward(40, true, 100), LEGENDARY);
+    EXPECT_EQ(calculateLoginAward(40, false, 100), BIG);
+    EXPECT_EQ(calculateLoginAward(70, true, 30), BIG);
+    EXPECT_EQ(calculateLoginAward(70, false, 30), NORMAL);
+    EXPECT_EQ(calculateLoginAward(30, true, 70), BIG);
+    EXPECT_EQ(calculateLoginAward(30, false, 70), NORMAL);
+    EXPECT_EQ(calculateLoginAward(30, true, 30), NORMAL);
+    EXPECT_EQ(calculateLoginAward(30, false, 30), NO);
 }
